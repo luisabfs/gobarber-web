8b2b15e524a8a7dec1ac827d370d375fb74ed444
Luisa Barros <qlullyq@gmail.com>
2020-11-16 19:03:48 -0300

    feat: create toast jsx structure

---
+++ src/components/toastContainer/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+
+import { Container, Toast } from './styles';
+
+const ToastContainer: React.FC = () => {
+  return (
+    <Container>
+      <Toast type="error" hasDescription>
+        <FiAlertCircle />
+
+        <div>
+          <strong>An error has happened.</strong>
+          <p>It was not possible to log in the app.</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+    </Container>
+  );
+};
+
+export default ToastContainer;
